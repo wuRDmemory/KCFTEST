49e2797555e95e5a7f71af93f8c0972c386475fb
wuRDmemory <WuCH2039@163.com>
2017-12-27 19:38:33 +0800

    原始程序

---
+++ cmake-build-debug/CMakeFiles/KCFTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KCFTest.dir/main.cpp.o"
+  "KCFTest.pdb"
+  "KCFTest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/KCFTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
